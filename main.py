936748b7f1c1297346762dcdfeea2fad17d06538
Gabriel Lozano <gabolozano1983@gmail.com>
2024-05-01 10:39:45 +1000

    Update main.py

--- main.py
+++ main.py
@@ -12,23 +12,23 @@ def save_email(email):
     with open("mails.txt", "a") as file:
         file.write(email + "\n")
 
-with st.sidebar:
-    st.write("Official demo for [Scrapegraph-ai](https://github.com/VinciGit00/Scrapegraph-ai) library")
-    st.markdown("""---""")
-    st.write("# Usage Examples")
-    st.write("## Prompt 1")
-    st.write("- Give me all the news with their abstracts")
-    st.write("## Prompt 2")
-    st.write("- Create a voice summary of the webpage")
-    st.write("## Prompt 3")
-    st.write("- List me all the images with their visual description")
-    st.write("## Prompt 4")
-    st.write("- Read me the summary of the news")
-    st.markdown("""---""")
-    st.write("You want to suggest tips or improvements? Contact me through email to mvincig11@gmail.com")
+#with st.sidebar:
+#    st.write("Official demo for [Scrapegraph-ai](https://github.com/VinciGit00/Scrapegraph-ai) library")
+#    st.markdown("""---""")
+#    st.write("# Usage Examples")
+#    st.write("## Prompt 1")
+#    st.write("- Give me all the news with their abstracts")
+#    st.write("## Prompt 2")
+#    st.write("- Create a voice summary of the webpage")
+#    st.write("## Prompt 3")
+#    st.write("- List me all the images with their visual description")
+#    st.write("## Prompt 4")
+#    st.write("- Read me the summary of the news")
+#    st.markdown("""---""")
+#    st.write("You want to suggest tips or improvements? Contact me through email to mvincig11@gmail.com")
 
 
-st.title("Scrapegraph-ai")
+st.title("Scrapegraph.ai Gab Test")
 left_co, cent_co, last_co = st.columns(3)
 with cent_co:
     st.image("assets/scrapegraphai_logo.png")
@@ -36,7 +36,8 @@ with cent_co:
 key = st.text_input("Openai API key", type="password")
 model = st.radio(
     "Select the model",
-    ["gpt-3.5-turbo", "gpt-3.5-turbo-0125", "gpt-4", "text-to-speech"],
+    ["gpt-3.5-turbo", "gpt-3.5-turbo-0125", "gpt-4"],
+#    ["gpt-3.5-turbo", "gpt-3.5-turbo-0125", "gpt-4", "text-to-speech"],
     index=0,
 )
 
@@ -79,34 +80,34 @@ if st.button("Run the program", type="primary"):
                     mime="text/csv"
                 )
 
-left_co2, *_, cent_co2, last_co2, last_c3= st.columns([1]*18)
+#left_co2, *_, cent_co2, last_co2, last_c3= st.columns([1]*18)
 
-with cent_co2:
-    discord_link = "https://discord.gg/DujC7HG8"
-    discord_logo = base64.b64encode(open("assets/discord.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{discord_link}" target="_blank">
-        <img src="data:image/png;base64,{discord_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
+#with cent_co2:
+#    discord_link = "https://discord.gg/DujC7HG8"
+#    discord_logo = base64.b64encode(open("assets/discord.png", "rb").read()).decode()
+#    st.markdown(
+#        f"""<a href="{discord_link}" target="_blank">
+#        <img src="data:image/png;base64,{discord_logo}" width="25">
+#        </a>""",
+#        unsafe_allow_html=True,
+#    )
 
-with last_co2:
-    github_link = "https://github.com/VinciGit00/Scrapegraph-ai"
-    github_logo = base64.b64encode(open("assets/github.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{github_link}" target="_blank">
-        <img src="data:image/png;base64,{github_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
+#with last_co2:
+#    github_link = "https://github.com/VinciGit00/Scrapegraph-ai"
+#    github_logo = base64.b64encode(open("assets/github.png", "rb").read()).decode()
+#    st.markdown(
+#        f"""<a href="{github_link}" target="_blank">
+#        <img src="data:image/png;base64,{github_logo}" width="25">
+#        </a>""",
+#        unsafe_allow_html=True,
+#    )
 
-with last_c3:
-    twitter_link = "https://twitter.com/scrapegraphai"
-    twitte_logo = base64.b64encode(open("assets/twitter.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{twitter_link}" target="_blank">
-        <img src="data:image/png;base64,{twitte_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
+#with last_c3:
+#    twitter_link = "https://twitter.com/scrapegraphai"
+#    twitte_logo = base64.b64encode(open("assets/twitter.png", "rb").read()).decode()
+#    st.markdown(
+#        f"""<a href="{twitter_link}" target="_blank">
+#        <img src="data:image/png;base64,{twitte_logo}" width="25">
+#        </a>""",
+#        unsafe_allow_html=True,
+#    )
